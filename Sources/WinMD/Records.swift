53b85fd9ac5d78b2a44d96555be0430b46d508bc
Tristan Labelle <git@tristanlabelle.com>
2023-05-08 14:43:06 -0400

    Reordered records structs alphabetically

--- Sources/WinMD/Records.swift
+++ Sources/WinMD/Records.swift
@@ -1,87 +1,63 @@
-public struct Module: Record {
-    public var generation: UInt16
+public struct Assembly: Record {
+    public var hashAlgId: AssemblyHashAlgorithm
+    public var majorVersion: UInt16, minorVersion: UInt16, buildNumber: UInt16, revisionNumber: UInt16
+    public var flags: AssemblyFlags
+    public var publicKey: HeapEntry<BlobHeap>
     public var name: HeapEntry<StringHeap>
-    public var mvid: HeapEntry<GuidHeap>
-    public var encId: HeapEntry<GuidHeap>
-    public var encBaseId: HeapEntry<GuidHeap>
+    public var culture: HeapEntry<StringHeap>
 
-    public static var tableIndex: TableIndex { .module }
+    public static var tableIndex: TableIndex { .assembly }
 
     public static func getSize(dimensions: Database.Dimensions) -> Int {
         RowSizer<Self>(dimensions: dimensions)
-            .addConstant(\.generation)
-            .addHeapEntry(\.name)
-            .addHeapEntry(\.mvid)
-            .addHeapEntry(\.encId)
-            .addHeapEntry(\.encBaseId)
-            .size
-    }
-
-    public static func read(buffer: UnsafeRawBufferPointer, dimensions: Database.Dimensions) -> Self {
-        var reader = ColumnReader(buffer: buffer, dimensions: dimensions)
-        return Self(
-            generation: reader.readConstant(),
-            name: reader.readHeapEntry(),
-            mvid: reader.readHeapEntry(),
-            encId: reader.readHeapEntry(),
-            encBaseId: reader.readHeapEntry(last: true))
-    }
-}
-
-public struct TypeRef: Record {
-    var resolutionScope: ResolutionScope
-    var typeName: HeapEntry<StringHeap>
-    var typeNamespace: HeapEntry<StringHeap>
-
-    public static var tableIndex: TableIndex { .typeRef }
-
-    public static func getSize(dimensions: Database.Dimensions) -> Int {
-        RowSizer<Self>(dimensions: dimensions)
-            .addCodedIndex(\.resolutionScope)
-            .addHeapEntry(\.typeName)
-            .addHeapEntry(\.typeNamespace)
-            .size
-    }
-
-    public static func read(buffer: UnsafeRawBufferPointer, dimensions: Database.Dimensions) -> Self {
-        var reader = ColumnReader(buffer: buffer, dimensions: dimensions)
-        return Self(
-            resolutionScope: reader.readConstant(),
-            typeName: reader.readHeapEntry(),
-            typeNamespace: reader.readHeapEntry(last: true))
-    }
-}
-
-public struct TypeDef: Record {
-    public var flags: TypeAttributes
-    public var typeName: HeapEntry<StringHeap>
-    public var typeNamespace: HeapEntry<StringHeap>
-    public var extends: TypeDefOrRef
-    public var fieldList: TableRow<Field>
-    public var methodList: TableRow<MethodDef>
-
-    public static var tableIndex: TableIndex { .typeDef }
-
-    public static func getSize(dimensions: Database.Dimensions) -> Int {
-        RowSizer<Self>(dimensions: dimensions)
+            .addConstant(\.hashAlgId)
+            .addConstant(\.majorVersion)
+            .addConstant(\.minorVersion)
+            .addConstant(\.buildNumber)
+            .addConstant(\.revisionNumber)
             .addConstant(\.flags)
-            .addHeapEntry(\.typeName)
-            .addHeapEntry(\.typeNamespace)
-            .addCodedIndex(\.extends)
-            .addTableRow(\.fieldList)
-            .addTableRow(\.methodList)
+            .addHeapEntry(\.publicKey)
+            .addHeapEntry(\.name)
+            .addHeapEntry(\.culture)
+            .size
+    }
+    
+    public static func read(buffer: UnsafeRawBufferPointer, dimensions: Database.Dimensions) -> Self {
+        var reader = ColumnReader(buffer: buffer, dimensions: dimensions)
+        return Self(
+            hashAlgId: reader.readConstant(),
+            majorVersion: reader.readConstant(),
+            minorVersion: reader.readConstant(),
+            buildNumber: reader.readConstant(),
+            revisionNumber: reader.readConstant(),
+            flags: reader.readConstant(),
+            publicKey: reader.readHeapEntry(),
+            name: reader.readHeapEntry(),
+            culture: reader.readHeapEntry(last: true))
+    }
+}
+
+public struct Constant: Record {
+    public var type: UInt16
+    public var parent: HasConstant
+    public var value: HeapEntry<BlobHeap>
+
+    public static var tableIndex: TableIndex { .constant }
+
+    public static func getSize(dimensions: Database.Dimensions) -> Int {
+        RowSizer<Self>(dimensions: dimensions)
+            .addConstant(\.type)
+            .addCodedIndex(\.parent)
+            .addHeapEntry(\.value)
             .size
     }
 
     public static func read(buffer: UnsafeRawBufferPointer, dimensions: Database.Dimensions) -> Self {
         var reader = ColumnReader(buffer: buffer, dimensions: dimensions)
         return Self(
-            flags: reader.readConstant(),
-            typeName: reader.readHeapEntry(),
-            typeNamespace: reader.readHeapEntry(),
-            extends: reader.readCodedIndex(),
-            fieldList: reader.readTableRow(),
-            methodList: reader.readTableRow(last: true))
+            type: reader.readConstant(),
+            parent: reader.readCodedIndex(),
+            value: reader.readHeapEntry(last: true))
     }
 }
 
@@ -106,63 +82,6 @@ public struct Field: Record {
             flags: reader.readConstant(),
             name: reader.readHeapEntry(),
             signature: reader.readHeapEntry(last: true))
-    }
-}
-
-public struct MethodDef: Record {
-    public var rva: UInt32
-    public var implFlags: MethodImplAttributes
-    public var flags: MethodAttributes
-    public var name: HeapEntry<StringHeap>
-    public var signature: HeapEntry<BlobHeap>
-    public var paramList: TableRow<Param>
-
-    public static var tableIndex: TableIndex { .methodDef }
-
-    public static func getSize(dimensions: Database.Dimensions) -> Int {
-        RowSizer<Self>(dimensions: dimensions)
-            .addConstant(\.rva)
-            .addConstant(\.implFlags)
-            .addConstant(\.flags)
-            .addHeapEntry(\.name)
-            .addHeapEntry(\.signature)
-            .addTableRow(\.paramList)
-            .size
-    }
-
-    public static func read(buffer: UnsafeRawBufferPointer, dimensions: Database.Dimensions) -> Self {
-        var reader = ColumnReader(buffer: buffer, dimensions: dimensions)
-        return Self(
-            rva: reader.readConstant(),
-            implFlags: reader.readConstant(),
-            flags: reader.readConstant(),
-            name: reader.readHeapEntry(),
-            signature: reader.readHeapEntry(),
-            paramList: reader.readTableRow(last: true))
-    }
-}
-
-public struct Param: Record {
-    public var flags: ParamAttributes
-    public var sequence: UInt16
-    public var name: HeapEntry<StringHeap>
-
-    public static var tableIndex: TableIndex { .param }
-
-    public static func getSize(dimensions: Database.Dimensions) -> Int {
-        RowSizer<Self>(dimensions: dimensions)
-            .addConstant(\.flags)
-            .addConstant(\.sequence)
-            .addHeapEntry(\.name)
-            .size
-    }
-
-    public static func read(buffer: UnsafeRawBufferPointer, dimensions: Database.Dimensions) -> Self {
-        var reader = ColumnReader(buffer: buffer, dimensions: dimensions)
-        return Self(
-            flags: reader.readConstant(),
-            sequence: reader.readConstant(),
-            name: reader.readHeapEntry(last: true))
     }
 }
 
@@ -211,65 +130,146 @@ public struct MemberRef: Record {
     }
 }
 
-public struct Constant: Record {
-    public var type: UInt16
-    public var parent: HasConstant
-    public var value: HeapEntry<BlobHeap>
+public struct MethodDef: Record {
+    public var rva: UInt32
+    public var implFlags: MethodImplAttributes
+    public var flags: MethodAttributes
+    public var name: HeapEntry<StringHeap>
+    public var signature: HeapEntry<BlobHeap>
+    public var paramList: TableRow<Param>
 
-    public static var tableIndex: TableIndex { .constant }
+    public static var tableIndex: TableIndex { .methodDef }
 
     public static func getSize(dimensions: Database.Dimensions) -> Int {
         RowSizer<Self>(dimensions: dimensions)
-            .addConstant(\.type)
-            .addCodedIndex(\.parent)
-            .addHeapEntry(\.value)
+            .addConstant(\.rva)
+            .addConstant(\.implFlags)
+            .addConstant(\.flags)
+            .addHeapEntry(\.name)
+            .addHeapEntry(\.signature)
+            .addTableRow(\.paramList)
             .size
     }
 
     public static func read(buffer: UnsafeRawBufferPointer, dimensions: Database.Dimensions) -> Self {
         var reader = ColumnReader(buffer: buffer, dimensions: dimensions)
         return Self(
-            type: reader.readConstant(),
-            parent: reader.readCodedIndex(),
-            value: reader.readHeapEntry(last: true))
+            rva: reader.readConstant(),
+            implFlags: reader.readConstant(),
+            flags: reader.readConstant(),
+            name: reader.readHeapEntry(),
+            signature: reader.readHeapEntry(),
+            paramList: reader.readTableRow(last: true))
     }
 }
 
-public struct Assembly: Record {
-    public var hashAlgId: AssemblyHashAlgorithm
-    public var majorVersion: UInt16, minorVersion: UInt16, buildNumber: UInt16, revisionNumber: UInt16
-    public var flags: AssemblyFlags
-    public var publicKey: HeapEntry<BlobHeap>
+public struct Module: Record {
+    public var generation: UInt16
     public var name: HeapEntry<StringHeap>
-    public var culture: HeapEntry<StringHeap>
+    public var mvid: HeapEntry<GuidHeap>
+    public var encId: HeapEntry<GuidHeap>
+    public var encBaseId: HeapEntry<GuidHeap>
 
-    public static var tableIndex: TableIndex { .assembly }
+    public static var tableIndex: TableIndex { .module }
 
     public static func getSize(dimensions: Database.Dimensions) -> Int {
         RowSizer<Self>(dimensions: dimensions)
-            .addConstant(\.hashAlgId)
-            .addConstant(\.majorVersion)
-            .addConstant(\.minorVersion)
-            .addConstant(\.buildNumber)
-            .addConstant(\.revisionNumber)
-            .addConstant(\.flags)
-            .addHeapEntry(\.publicKey)
+            .addConstant(\.generation)
             .addHeapEntry(\.name)
-            .addHeapEntry(\.culture)
+            .addHeapEntry(\.mvid)
+            .addHeapEntry(\.encId)
+            .addHeapEntry(\.encBaseId)
             .size
     }
-    
+
     public static func read(buffer: UnsafeRawBufferPointer, dimensions: Database.Dimensions) -> Self {
         var reader = ColumnReader(buffer: buffer, dimensions: dimensions)
         return Self(
-            hashAlgId: reader.readConstant(),
-            majorVersion: reader.readConstant(),
-            minorVersion: reader.readConstant(),
-            buildNumber: reader.readConstant(),
-            revisionNumber: reader.readConstant(),
-            flags: reader.readConstant(),
-            publicKey: reader.readHeapEntry(),
+            generation: reader.readConstant(),
             name: reader.readHeapEntry(),
-            culture: reader.readHeapEntry(last: true))
+            mvid: reader.readHeapEntry(),
+            encId: reader.readHeapEntry(),
+            encBaseId: reader.readHeapEntry(last: true))
+    }
+}
+
+public struct Param: Record {
+    public var flags: ParamAttributes
+    public var sequence: UInt16
+    public var name: HeapEntry<StringHeap>
+
+    public static var tableIndex: TableIndex { .param }
+
+    public static func getSize(dimensions: Database.Dimensions) -> Int {
+        RowSizer<Self>(dimensions: dimensions)
+            .addConstant(\.flags)
+            .addConstant(\.sequence)
+            .addHeapEntry(\.name)
+            .size
+    }
+
+    public static func read(buffer: UnsafeRawBufferPointer, dimensions: Database.Dimensions) -> Self {
+        var reader = ColumnReader(buffer: buffer, dimensions: dimensions)
+        return Self(
+            flags: reader.readConstant(),
+            sequence: reader.readConstant(),
+            name: reader.readHeapEntry(last: true))
+    }
+}
+
+public struct TypeDef: Record {
+    public var flags: TypeAttributes
+    public var typeName: HeapEntry<StringHeap>
+    public var typeNamespace: HeapEntry<StringHeap>
+    public var extends: TypeDefOrRef
+    public var fieldList: TableRow<Field>
+    public var methodList: TableRow<MethodDef>
+
+    public static var tableIndex: TableIndex { .typeDef }
+
+    public static func getSize(dimensions: Database.Dimensions) -> Int {
+        RowSizer<Self>(dimensions: dimensions)
+            .addConstant(\.flags)
+            .addHeapEntry(\.typeName)
+            .addHeapEntry(\.typeNamespace)
+            .addCodedIndex(\.extends)
+            .addTableRow(\.fieldList)
+            .addTableRow(\.methodList)
+            .size
+    }
+
+    public static func read(buffer: UnsafeRawBufferPointer, dimensions: Database.Dimensions) -> Self {
+        var reader = ColumnReader(buffer: buffer, dimensions: dimensions)
+        return Self(
+            flags: reader.readConstant(),
+            typeName: reader.readHeapEntry(),
+            typeNamespace: reader.readHeapEntry(),
+            extends: reader.readCodedIndex(),
+            fieldList: reader.readTableRow(),
+            methodList: reader.readTableRow(last: true))
+    }
+}
+
+public struct TypeRef: Record {
+    var resolutionScope: ResolutionScope
+    var typeName: HeapEntry<StringHeap>
+    var typeNamespace: HeapEntry<StringHeap>
+
+    public static var tableIndex: TableIndex { .typeRef }
+
+    public static func getSize(dimensions: Database.Dimensions) -> Int {
+        RowSizer<Self>(dimensions: dimensions)
+            .addCodedIndex(\.resolutionScope)
+            .addHeapEntry(\.typeName)
+            .addHeapEntry(\.typeNamespace)
+            .size
+    }
+
+    public static func read(buffer: UnsafeRawBufferPointer, dimensions: Database.Dimensions) -> Self {
+        var reader = ColumnReader(buffer: buffer, dimensions: dimensions)
+        return Self(
+            resolutionScope: reader.readConstant(),
+            typeName: reader.readHeapEntry(),
+            typeNamespace: reader.readHeapEntry(last: true))
     }
 }
