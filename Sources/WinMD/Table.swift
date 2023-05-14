5bc5bd4f58b3420b08281b248ab88b2d1cf0e7dc
Tristan Labelle <tristan@thebrowser.company>
2023-05-14 19:22:36 -0400

    Moved TableRowIndex to its own file

--- Sources/WinMD/Table.swift
+++ Sources/WinMD/Table.swift
@@ -29,34 +29,3 @@ extension Table: Collection {
     public var endIndex: Int { count }
     public func index(after i: Int) -> Int { i + 1 }
 }
-
-public struct TableRowIndex<T>: Comparable where T: TableRow {
-    public static var tableIndex: TableIndex { T.tableIndex }
-    public static var null: Self { .init(oneBased: 0) }
-
-    public var oneBased: UInt32
-
-    public init(oneBased: UInt32) {
-        precondition(oneBased < 0x1_00_00_00)
-        self.oneBased = oneBased
-    }
-
-    public init(zeroBased: Int) {
-        precondition(zeroBased < 0xFF_FF_FF)
-        self.init(oneBased: UInt32(zeroBased + 1))
-    }
-
-    public init(zeroBased: Int?) {
-        self.init(oneBased: UInt32((zeroBased ?? -1) + 1))
-    }
-
-    public var isNull: Bool { oneBased == 0 }
-    public var zeroBased: Int? { oneBased > 0 ? Int(oneBased - 1) : nil }
-
-    public static func < (lhs: TableRowIndex, rhs: TableRowIndex) -> Bool {
-        // 0, the null row, is considered last
-        if (lhs.isNull) { return false }
-        if (rhs.isNull) { return true }
-        return lhs.oneBased < rhs.oneBased
-    }
-}
