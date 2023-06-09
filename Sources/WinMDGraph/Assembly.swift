262ba7aeadd837ae7aad22b0fa16cef347f9a24d
Tristan Labelle <git@tristanlabelle.com>
2023-06-09 07:04:50 -0400

    Added Assembly.debugDescription

--- Sources/WinMDGraph/Assembly.swift
+++ Sources/WinMDGraph/Assembly.swift
@@ -1,6 +1,6 @@
 import WinMD
 
-public class Assembly {
+public class Assembly: CustomDebugStringConvertible {
     typealias Impl = AssemblyImpl
 
     public let context: MetadataContext
@@ -16,7 +16,13 @@ public class Assembly {
     public var version: AssemblyVersion { impl.version }
     public var culture: String { impl.culture }
     public var types: [TypeDefinition] { impl.types }
-    
+
+    public var debugDescription: String {
+        var result = "\(name), Version=\(version)"
+        if !culture.isEmpty { result += ", Culture=\(culture)" }
+        return result
+    }
+
     public private(set) lazy var typesByFullName: [String: TypeDefinition] = {
         Dictionary(uniqueKeysWithValues: types.map { ($0.fullName, $0) })
     }()
