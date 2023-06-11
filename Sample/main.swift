74b25c27131d09e1ba2b904ba31be814c42fcb02
Tristan Labelle <git@tristanlabelle.com>
2023-06-11 08:04:32 -0400

    Fixed main.swift.

--- Sample/main.swift
+++ Sample/main.swift
@@ -1,22 +1,15 @@
 import struct Foundation.URL
-import DotNetMDPhysical
 import DotNetMDLogical
 
 struct AssemblyNotFound: Error {}
 
-let url = URL(fileURLWithPath: #"C:\Program Files (x86)\Windows Kits\10\UnionMetadata\10.0.22000.0\Windows.DotNetMDPhysical"#)
+let url = URL(fileURLWithPath: #"C:\Program Files (x86)\Windows Kits\10\UnionMetadata\10.0.22000.0\Windows.winmd"#)
 
-let database = try Database(url: url)
-let assemblyRef = database.tables.assemblyRef[0]
-let name = database.heaps.resolve(database.tables.assemblyRef[0].name)
-print("\(name) \(assemblyRef.majorVersion).\(assemblyRef.minorVersion).\(assemblyRef.buildNumber).\(assemblyRef.revisionNumber)")
-
-// let context = MetadataContext(assemblyResolver: { _ in throw AssemblyNotFound() })
-// let assembly = try context.loadAssembly(url: url)
-// print("Loaded assembly: \(assembly.name)")
-// let iclosable = assembly.findTypeDefinition(fullName: "Windows.Foundation.IClosable")!
-// print("interface IClosable {")
-// for method in iclosable.methods {
-//     print("  void \(method.name)")
-// }
-// print("}")
+let context = MetadataContext(assemblyResolver: { _ in throw AssemblyNotFound() })
+let assembly = try context.loadAssembly(url: url)
+let iclosable = assembly.findTypeDefinition(fullName: "Windows.Foundation.IClosable")!
+print("interface IClosable {")
+for method in iclosable.methods {
+    print("  void \(method.name)()")
+}
+print("}")
