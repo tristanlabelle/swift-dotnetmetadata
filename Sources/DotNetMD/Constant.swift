951671793552b5eafc5573cf4c97daae367484dc
Tristan Labelle <git@tristanlabelle.com>
2023-06-15 06:58:26 -0400

    Added Constant enum

---
+++ Sources/DotNetMD/Constant.swift
@@ -0,0 +1,16 @@
+public enum Constant {
+    case boolean(Bool)
+    case char(UInt16)
+    case i1(Int8)
+    case u1(UInt8)
+    case i2(Int16)
+    case u2(UInt16)
+    case i4(Int32)
+    case u4(UInt32)
+    case i8(Int64)
+    case u8(UInt64)
+    case r4(Float)
+    case r8(Double)
+    case string(String)
+    case null
+}
