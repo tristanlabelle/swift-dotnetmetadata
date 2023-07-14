27de6ec4c643f28bfef22ac8f5c015a61e8b4d68
Tristan Labelle <git@tristanlabelle.com>
2023-07-14 06:16:34 -0400

    Update WinMD tests to use WinMD files from the system's WinMetadata directory.

---
+++ Tests/UnitTests/SystemWinMetadataWinMDTests.swift
@@ -0,0 +1,28 @@
+import Foundation
+import XCTest
+@testable import DotNetMD
+
+final class SystemWinMetadataWinMDTests: XCTestCase {
+    internal static var context: MetadataContext!
+    internal static var assembly: Assembly!
+
+    override class func setUp() {
+        let windowsPath = ProcessInfo.processInfo.environment["windir"]
+            ?? ProcessInfo.processInfo.environment["SystemRoot"]
+            ?? #"C:\Windows"#
+        let url = URL(fileURLWithPath: "\(windowsPath)\\System32\\WinMetadata\\Windows.Foundation.winmd")
+
+        context = MetadataContext()
+        assembly = try? context.loadAssembly(url: url)
+    }
+
+    override func setUpWithError() throws {
+        try XCTSkipIf(Self.assembly == nil, "System Windows.Foundation.winmd not found")
+    }
+
+    func testMscorlibTypeReference() throws {
+        XCTAssertEqual(
+            Self.assembly.findDefinedType(fullName: "Windows.Foundation.Point")?.unboundBase?.fullName,
+            "System.ValueType")
+    }
+}
