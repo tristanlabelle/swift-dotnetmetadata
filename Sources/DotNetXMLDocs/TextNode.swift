a59a4ca47f3e6872ae99d11d97f4217de9b7e9a2
Tristan Labelle <git@tristanlabelle.com>
2023-08-12 17:38:26 -0400

    Added more documentation TextNode cases.

--- Sources/DotNetXMLDocs/TextNode.swift
+++ Sources/DotNetXMLDocs/TextNode.swift
@@ -1,5 +1,18 @@
 public enum TextNode: Hashable {
     case plain(String)
     case sequence([TextNode])
-    case list(items: [TextNode])
+
+    // Block formatting
+    indirect case paragraph(TextNode) // <para></para>
+    case list(items: [TextNode]) // <list><item></item></list>
+    case codeBlock(String) // <code></code>
+    indirect case example(TextNode) // <example></example>
+
+    // Inline formatting
+    case codeSpan(name: String) // <c></c>
+
+    // References
+    case paramReference(name: String) // <paramref name=""/>
+    case typeParamReference(name: String) // <typeparamref name=""/>
+    case see(codeReference: MemberKey, url: String? = nil, also: Bool = false) // <see[also] cref="" url=""/>
 }
