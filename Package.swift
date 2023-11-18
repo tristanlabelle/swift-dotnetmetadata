a2cf4cbb6b11a29fa847ec72e161739668cabaa8
Tristan Labelle <git@tristanlabelle.com>
2023-11-18 07:58:26 -0500

    Split unit tests into multiple modules.

--- Package.swift
+++ Package.swift
@@ -2,6 +2,9 @@
 
 import PackageDescription
 
+// Workaround for SPM library support limitations causing "LNK4217: locally defined symbol imported" spew
+let executableLinkerSettings = [ LinkerSetting.unsafeFlags(["-Xlinker", "-ignore:4217"]) ]
+
 let package = Package(
     name: "DotNetMetadata",
     products: [
@@ -12,21 +15,40 @@ let package = Package(
     targets: [
         .target(
             name: "CInterop"),
+
         .target(
             name: "DotNetMetadataFormat",
             dependencies: [ "CInterop" ]),
+        .testTarget(
+            name: "DotNetMetadataFormatTests",
+            dependencies: [ "DotNetMetadataFormat" ],
+            path: "Tests/DotNetMetadataFormat",
+            linkerSettings: executableLinkerSettings),
+
         .target(
             name: "DotNetMetadata",
             dependencies: [ "DotNetMetadataFormat" ]),
+        .testTarget(
+            name: "DotNetMetadataTests",
+            dependencies: [ "DotNetMetadata" ],
+            path: "Tests/DotNetMetadata",
+            linkerSettings: executableLinkerSettings),
+
         .target(
             name: "WindowsMetadata",
             dependencies: [ "DotNetMetadata" ]),
+        .testTarget(
+            name: "WindowsMetadataTests",
+            dependencies: [ "WindowsMetadata" ],
+            path: "Tests/WindowsMetadata",
+            linkerSettings: executableLinkerSettings),
+
         .target(
             name: "DotNetXMLDocs"),
         .testTarget(
-            name: "UnitTests",
-            dependencies: [ "DotNetMetadataFormat", "DotNetMetadata", "DotNetXMLDocs", "WindowsMetadata" ],
-            // Workaround for SPM library support limitations causing "LNK4217: locally defined symbol imported" spew
-            linkerSettings: [ .unsafeFlags(["-Xlinker", "-ignore:4217"]) ])
+            name: "DotNetXMLDocsTests",
+            dependencies: [ "DotNetXMLDocs" ],
+            path: "Tests/DotNetXMLDocs",
+            linkerSettings: executableLinkerSettings),
     ]
 )
