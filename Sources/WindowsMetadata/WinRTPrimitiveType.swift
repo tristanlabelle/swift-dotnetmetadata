8baf7e72ce4127f39067d1807e0d57a34a5af4d4
Tristan Labelle <git@tristanlabelle.com>
2024-03-24 08:06:37 -0400

    Added extensions to WinRTPrimitiveType for shortened names.

--- Sources/WindowsMetadata/WinRTPrimitiveType.swift
+++ Sources/WindowsMetadata/WinRTPrimitiveType.swift
@@ -10,6 +10,18 @@ public enum WinRTPrimitiveType: Hashable {
     case string
 }
 
+extension WinRTPrimitiveType {
+    public static var uint8: WinRTPrimitiveType { .integer(.uint8) }
+    public static var int16: WinRTPrimitiveType { .integer(.int16)  }
+    public static var uint16: WinRTPrimitiveType { .integer(.uint16) }
+    public static var int32: WinRTPrimitiveType { .integer(.int32) }
+    public static var uint32: WinRTPrimitiveType { .integer(.uint32) }
+    public static var int64: WinRTPrimitiveType { .integer(.int64) }
+    public static var uint64: WinRTPrimitiveType { .integer(.uint64) }
+    public static var single: WinRTPrimitiveType { .float(double: false) }
+    public static var double: WinRTPrimitiveType { .float(double: true) }
+}
+
 extension WinRTPrimitiveType: CustomStringConvertible {
     public var name: String {
         switch self {
@@ -29,15 +41,15 @@ extension WinRTPrimitiveType {
     public init?(fromName name: String) {
         switch name {
             case "Boolean": self = .boolean
-            case "Byte": self = .integer(.uint8)
-            case "Int16": self = .integer(.int16)
-            case "UInt16": self = .integer(.uint16)
-            case "Int32": self = .integer(.int32)
-            case "UInt32": self = .integer(.uint32)
-            case "Int64": self = .integer(.int64)
-            case "UInt64": self = .integer(.uint64)
-            case "Single": self = .float(double: false)
-            case "Double": self = .float(double: true)
+            case "Byte": self = .uint8
+            case "Int16": self = .int16
+            case "UInt16": self = .uint16
+            case "Int32": self = .int32
+            case "UInt32": self = .uint32
+            case "Int64": self = .int64
+            case "UInt64": self = .uint64
+            case "Single": self = .single
+            case "Double": self = .double
             case "Char16": self = .char16
             case "Guid": self = .guid
             case "String": self = .string
