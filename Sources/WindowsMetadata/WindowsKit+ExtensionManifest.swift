36f807a581b1a017621e074ca5568e7ca2573d05
Tristan Labelle <git@tristanlabelle.com>
2024-08-13 20:04:30 -0400

    Made test pass

--- Sources/WindowsMetadata/WindowsKit+ExtensionManifest.swift
+++ Sources/WindowsMetadata/WindowsKit+ExtensionManifest.swift
@@ -7,7 +7,7 @@ extension WindowsKit {
     public struct ExtensionManifest {
         public var targetPlatform: String
         public var targetPlatformMinVersion: FourPartVersion
-        public var platformMinVersion: FourPartVersion
+        public var targetPlatformVersion: FourPartVersion
         public var sdkType: String
         public var displayName: String
         public var appliesTo: String
@@ -24,13 +24,13 @@ extension WindowsKit {
             guard let rootElement = document.rootElement(), rootElement.name == "FileList" else {
                 fatalError()
             }
-            self.targetPlatform = rootElement.attribute(forName: "targetPlatform")!.stringValue!
-            self.targetPlatformMinVersion = try FourPartVersion(parsing: rootElement.attribute(forName: "friendlyName")!.stringValue!)
-            self.platformMinVersion = try FourPartVersion(parsing: rootElement.attribute(forName: "platformMinVersion")!.stringValue!)
-            self.sdkType = rootElement.attribute(forName: "sdkType")!.stringValue!
-            self.displayName = rootElement.attribute(forName: "displayName")!.stringValue!
-            self.appliesTo = rootElement.attribute(forName: "appliesTo")!.stringValue!
-            self.productFamilyName = rootElement.attribute(forName: "productFamilyName")!.stringValue!
+            self.targetPlatform = rootElement.attribute(forName: "TargetPlatform")!.stringValue!
+            self.targetPlatformMinVersion = try FourPartVersion(parsing: rootElement.attribute(forName: "TargetPlatformMinVersion")!.stringValue!)
+            self.targetPlatformVersion = try FourPartVersion(parsing: rootElement.attribute(forName: "TargetPlatformVersion")!.stringValue!)
+            self.sdkType = rootElement.attribute(forName: "SDKType")!.stringValue!
+            self.displayName = rootElement.attribute(forName: "DisplayName")!.stringValue!
+            self.appliesTo = rootElement.attribute(forName: "AppliesTo")!.stringValue!
+            self.productFamilyName = rootElement.attribute(forName: "ProductFamilyName")!.stringValue!
             self.apiContracts = [:]
             for apiContractElement in rootElement.singleElement(forName: "ContainedApiContracts")!.elements(forName: "ApiContract") {
                 let name = apiContractElement.attribute(forName: "name")!.stringValue!
