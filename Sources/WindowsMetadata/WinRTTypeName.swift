bcaa0cfb9ae68f306fe23761f290df2cb1155ad9
Tristan Labelle <git@tristanlabelle.com>
2024-10-26 10:17:07 -0400

    Remove arity suffixes from WinRTTypeName (#46)

--- Sources/WindowsMetadata/WinRTTypeName.swift
+++ Sources/WindowsMetadata/WinRTTypeName.swift
@@ -6,39 +6,40 @@ public enum WinRTTypeName: Hashable {
     // https://learn.microsoft.com/en-us/uwp/winrt-cref/winrt-type-system
     // > All types—except for the fundamental types—must be contained within a namespace.
     // > It's not valid for a type to be in the global namespace.
+    // WinRT types do not support nesting either.
     case declared(namespace: String, name: String)
 }
 
 extension WinRTTypeName: CustomStringConvertible, TextOutputStreamable {
-    public var description: String {
-        switch self {
-            case .object: return "Object"
-            case let .primitive(primitiveType): return primitiveType.name
-            default:
-                var output = String()
-                write(to: &output)
-                return output
-        }
+    public var description: String { toString() }
+
+    public func toString(useIInspectable: Bool = false, useAritySuffixes: Bool = false) -> String {
+        var output = String()
+        write(useIInspectable: useIInspectable, useAritySuffixes: useAritySuffixes, to: &output)
+        return output
     }
 
     public func write(to output: inout some TextOutputStream) {
+        write(useIInspectable: false, useAritySuffixes: false, to: &output)
+    }
+
+    public func write(useIInspectable: Bool = false, useAritySuffixes: Bool = false, to output: inout some TextOutputStream) {
         switch self {
             case .object:
-                output.write("Object")
+                output.write(useIInspectable ? "IInspectable" : "Object")
             case let .primitive(primitiveType):
                 output.write(primitiveType.name)
             case let .parameterized(type, args: args):
-                write(namespace: type.namespace, name: type.nameWithAritySuffix, genericArgs: args, to: &output)
+                let name = useAritySuffixes ? type.nameWithAritySuffix : type.nameWithoutAritySuffix
+                write(namespace: type.namespace, name: name, genericArgs: args, to: &output)
             case let .declared(namespace, name):
                 write(namespace: namespace, name: name, genericArgs: [], to: &output)
         }
     }
 
-    private func write(namespace: String?, name: String, genericArgs: [WinRTTypeName], to output: inout some TextOutputStream) {
-        if let namespace = namespace {
-            output.write(namespace)
-            output.write(".")
-        }
+    private func write(namespace: String, name: String, genericArgs: [WinRTTypeName], to output: inout some TextOutputStream) {
+        output.write(namespace)
+        output.write(".")
         output.write(name)
         if !genericArgs.isEmpty {
             output.write("<")
